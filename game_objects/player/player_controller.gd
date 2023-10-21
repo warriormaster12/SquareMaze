60950f4668899edb33e7a12739d8c95b3281e390
warriormaster12 <streng.alexander@outlook.com>
2023-10-21 19:27:05 +0300

    Improve the game

--- game_objects/player/player_controller.gd
+++ game_objects/player/player_controller.gd
@@ -2,32 +2,72 @@ extends Node2D
 class_name PlayerController2D
 
 @export var speed:float = 300
+@export var ray_length: float = 1000
 
 @onready var player_sprite: Sprite2D = $PlayerSprite
-var level_info: LevelInfo = null
+@onready var ray_down: RayCast2D = $DownRay
+@onready var ray_up: RayCast2D = $UpRay
+@onready var ray_left: RayCast2D = $LeftRay
+@onready var ray_right: RayCast2D = $RightRay
+@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
 var stop_moving: bool = false
 
 var move_tween: Tween = null
-var direction: Vector2 = Vector2.ZERO
 var last_pos: Vector2 = Vector2.ZERO
+var direction: Vector2 = Vector2.ZERO
+
+var selected_ray:RayCast2D = null
+var can_move: bool = true
 
 func _ready() -> void:
-	level_info = get_owner()
+	ray_down.target_position *= (player_sprite.texture.get_width() * 0.5)
+	ray_up.target_position *= (player_sprite.texture.get_width() * 0.5)
+	ray_left.target_position *= (player_sprite.texture.get_width() * 0.5)
+	ray_right.target_position *= (player_sprite.texture.get_width() * 0.5)
 
+func _process(_delta: float) -> void:
+	if selected_ray:
+		if selected_ray.is_colliding():
+			var next_pos: Vector2 = selected_ray.get_collision_point() + selected_ray.get_collision_normal() * (player_sprite.texture.get_width() * 0.5)
+			global_position = next_pos
+			direction = Vector2.ZERO
 
-func _input(_event: InputEvent) -> void:
-	direction = Input.get_vector("move_l", "move_r", "move_u", "move_d")
-	if direction.length() > 0:
-		var next_pos: Vector2 = level_info.find_next_pos(direction)
-		if next_pos.length() == 0:
-			return
-		var duration: float = next_pos.distance_to(global_position) / speed
-		last_pos = global_position
-		move_tween = create_tween()
-		await move_tween.tween_property(self, "global_position", next_pos, duration).set_trans(Tween.TRANS_LINEAR).finished
+func _physics_process(delta: float) -> void:
+	if direction.length() == 0 && can_move:
+		if Input.is_action_just_pressed("move_r"):
+			if !ray_right.is_colliding():
+				audio_player.play()
+				direction.x = 1
+				last_pos = global_position
+				selected_ray = ray_right
+		if Input.is_action_just_pressed("move_l"):
+			if !ray_left.is_colliding():
+				audio_player.play()
+				direction.x = -1
+				last_pos = global_position
+				selected_ray = ray_left
+		if Input.is_action_just_pressed("move_u"):
+			if !ray_up.is_colliding():
+				audio_player.play()
+				direction.y = -1
+				last_pos = global_position
+				selected_ray = ray_up
+		if Input.is_action_just_pressed("move_d"):
+			if !ray_down.is_colliding():
+				audio_player.play()
+				direction.y = 1
+				last_pos = global_position
+				selected_ray = ray_down
+	
+	global_position += direction * speed * delta
 
 func on_hit() -> void:
-	if move_tween:
-		move_tween.stop()
+	direction = Vector2.ZERO
+	can_move = false
+	anim_player.play("hit")
+	await anim_player.animation_finished
+	anim_player.play_backwards("hit")
 	global_position = last_pos
-	level_info.last_pos()
+	await anim_player.animation_finished
+	can_move = true
--- game_objects/reward/reward.gd
+++ game_objects/reward/reward.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var normal_trap_container: Node2D = null
 @export var back_track_trap_container: Node2D = null
+@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _ready() -> void:
 	if !normal_trap_container && !back_track_trap_container: 
@@ -11,9 +12,15 @@ func _ready() -> void:
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.get_owner().is_in_group("player"):
+		for child in back_track_trap_container.get_children():
+			child.request_ready()
 		back_track_trap_container.process_mode = Node.PROCESS_MODE_INHERIT
-		back_track_trap_container.request_ready()
 		back_track_trap_container.visible = true
 		normal_trap_container.visible = false
 		normal_trap_container.process_mode = Node.PROCESS_MODE_DISABLED
-		queue_free()
+		visible = false
+		audio_player.play()
+
+
+func _on_audio_stream_player_2d_finished() -> void:
+	queue_free()
